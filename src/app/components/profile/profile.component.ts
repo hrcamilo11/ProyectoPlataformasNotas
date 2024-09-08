04d43b1564f63f887b1f0eaac1b78565323bad68
Camilo Hernández Romero <hrcamilo11@gmail.com>
2024-09-07 19:34:09 -0500

    Fixed: Home

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {CargarScriptsService} from "../../services/cargar-scripts.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-profile',
@@ -8,5 +10,9 @@ import { Component } from '@angular/core';
   styleUrl: './profile.component.css'
 })
 export class ProfileComponent {
+  constructor(_CargarScript:CargarScriptsService, private router: Router) {
 
+    _CargarScript.Carga(['profile/profile']);
+
+  }
 }
