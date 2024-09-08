3bb832bceca6f7ac495a8718f5f9024d8af2482f
TomasGutierrezO <tomasgo2004@gmail.com>
2024-09-07 19:55:47 -0700

    Add: Profile Component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,6 +6,7 @@ import {LoginComponent} from "./components/login/login.component";
 import {RecoveryPasswordComponent} from "./components/recovery-password/recovery-password.component";
 import {StartComponent} from "./components/start/start.component";
 import {HomeComponent} from "./components/home/home.component";
+import {ProfileComponent} from "./components/profile/profile.component";
 
 
 export const routes: Routes = [
@@ -13,6 +14,7 @@ export const routes: Routes = [
   { path: 'home', component: HomeComponent, canActivate: [homeGuard] }, // Usa el guard para la página de inicio
   {path: 'login', component: LoginComponent, canActivate: [authGuard]},
   {path: 'recovery', component: RecoveryPasswordComponent, canActivate: [authGuard]},
+  {path: 'profile', component: ProfileComponent, canActivate: [homeGuard] },
   { path: '**', redirectTo: '' }, // Redirige a la página de inicio si la ruta no se encuentra
 
 ];
